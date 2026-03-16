91262f8f55ce4909e78af56851607f3a278a83b4
ishan-saraswat-68 <ishansaraswat68@gmail.com>
2026-03-16 17:09:08 +0530

    fastapi setup

--- rag_queue/server.py
+++ rag_queue/server.py
@@ -3,11 +3,20 @@ from dotenv import load_dotenv
 load_dotenv()
 
 from fastapi import FastAPI, HTTPException, Query
+from fastapi.middleware.cors import CORSMiddleware
 
 from .client.rq_client import enqueue_query, redis_conn
 
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
